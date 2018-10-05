26589bd8dfdb49cd5aa7dd964a83ab3abb9b2939
tranerius <36367029+tranerius@users.noreply.github.com>
2018-10-05 23:21:14 +0200

    Мелкие правки

--- Arduino скетч/Sketch/Sketch.ino
+++ Arduino скетч/Sketch/Sketch.ino
@@ -7,15 +7,13 @@
 #define RELAY_POWER 2 // пин, питающий реле
 #define RELAY_SIGNAL 3 // пин, управляющий реле
 
-enum Modes{
-  StaticColor,
-  Animation,
-  Ambilight,
-  PolishFlag,
-  NotConnected
+enum Modes {
+	StaticColor,
+	Animation,
+	Ambilight,
+	PolishFlag,
+	NotConnected
 };
-
-byte static_color[3] = {0, 0, 0}; // цвет для StaticColor режима
 
 byte mode = Modes::NotConnected;
 
@@ -28,57 +26,90 @@ byte startIndex = 0;
 
 CRGB leds[NUM_LEDS]; // лента
 
+byte static_color[3]; // цвет для StaticColor режима
+
 void setup() {
-  Serial.begin(1000000);
-  FastLED.addLeds<NEOPIXEL, LED_CONTROL_PIN>(leds, (2 * VERTICAL_LEDS_NUM + 2 * HORIZONTAL_LEDS_NUM));
-  pinMode(RELAY_POWER, OUTPUT);
-  digitalWrite(RELAY_POWER, HIGH);
-  pinMode(RELAY_SIGNAL, OUTPUT);
-  RelayOn(false);
+	Serial.begin(1000000);
+	FastLED.addLeds<NEOPIXEL, LED_CONTROL_PIN>(leds, (2 * VERTICAL_LEDS_NUM + 2 * HORIZONTAL_LEDS_NUM));
+	pinMode(RELAY_POWER, OUTPUT);
+	digitalWrite(RELAY_POWER, HIGH);
+	pinMode(RELAY_SIGNAL, OUTPUT);
+	RelayOn(false);
 }
 
 void loop() {
-  if(mode == Modes::NotConnected) Serial.write("LED Strip\n");
-  
-  
+	if (mode == Modes::NotConnected) Serial.write("LED Strip\n");
+
+	if (Serial.available() > 0) {
+		if (millis() - time_als > 800) {
+			mode = Serial.read();
+			if (mode != Modes::NotConnected) RelayOn(true);
+			time_als = millis();
+		}
+
+		if (mode == Modes::StaticColor) {
+			byte static_R = static_color[0];
+			byte static_G = static_color[1];
+			byte static_B = static_color[2];
+			long temp_time = millis();
+			for (byte i = 0; i < 3; ++i) {
+				while (Serial.available() < 1 && millis() - temp_time < 10);
+				if (Serial.available() > 0) {
+					static_color[i] = Serial.read();
+					temp_time = millis();
+				}
+				else {
+					static_color[0] = static_R;
+					static_color[1] = static_G;
+					static_color[2] = static_B;
+					break;
+				}
+			}
+			StaticColorMode();
+		}
+		else if (mode == Modes::PolishFlag) PolishFlagMode();
+		else if (mode == Modes::NotConnected) RelayOn(false);
+	}
+
+	if (mode == Modes::Animation) AnimationMode();
 }
 
-void RelayOn(bool on){
-  if(on) digitalWrite(RELAY_SIGNAL, HIGH);
-  else digitalWrite(RELAY_SIGNAL, LOW);
+void RelayOn(bool on) {
+	if (on) digitalWrite(RELAY_SIGNAL, HIGH);
+	else digitalWrite(RELAY_SIGNAL, LOW);
 }
 
-void StaticColorMode(){
-  for(int i = 0; i < NUM_LEDS; ++i){
-    leds[i].setRGB(static_color[0], static_color[1], static_color[2]);
-  }
-  FastLED.show();
+void StaticColorMode() {
+	for (int i = 0; i < NUM_LEDS; ++i) {
+		leds[i].setRGB(static_color[0], static_color[1], static_color[2]);
+	}
+	FastLED.show();
 }
 
-void PolishFlagMode(){
-  for(int i=0; i < VERTICAL_LEDS_NUM / 2; ++i){
-    leds[i].setRGB(255, 0, 0);
-    leds[2 * VERTICAL_LEDS_NUM + HORIZONTAL_LEDS_NUM -1 - i].setRGB(255, 0, 0);
-  }
-  for(int i = VERTICAL_LEDS_NUM / 2; i < VERTICAL_LEDS_NUM; ++i){
-    leds[i].setRGB(255, 255, 255);
-    leds[2 * VERTICAL_LEDS_NUM + HORIZONTAL_LEDS_NUM -1 - i].setRGB(255, 255, 255);
-  }
-  for(int i = VERTICAL_LEDS_NUM; i < VERTICAL_LEDS_NUM + HORIZONTAL_LEDS_NUM; ++i){
-    leds[i].setRGB(255, 255, 255);
-    leds[VERTICAL_LEDS_NUM + HORIZONTAL_LEDS_NUM + i].setRGB(255, 0, 0);
-  }
-  FastLED.show();
+void PolishFlagMode() {
+	for (int i = 0; i < VERTICAL_LEDS_NUM / 2; ++i) {
+		leds[i].setRGB(255, 0, 0);
+		leds[2 * VERTICAL_LEDS_NUM + HORIZONTAL_LEDS_NUM - 1 - i].setRGB(255, 0, 0);
+	}
+	for (int i = VERTICAL_LEDS_NUM / 2; i < VERTICAL_LEDS_NUM; ++i) {
+		leds[i].setRGB(255, 255, 255);
+		leds[2 * VERTICAL_LEDS_NUM + HORIZONTAL_LEDS_NUM - 1 - i].setRGB(255, 255, 255);
+	}
+	for (int i = VERTICAL_LEDS_NUM; i < VERTICAL_LEDS_NUM + HORIZONTAL_LEDS_NUM; ++i) {
+		leds[i].setRGB(255, 255, 255);
+		leds[VERTICAL_LEDS_NUM + HORIZONTAL_LEDS_NUM + i].setRGB(255, 0, 0);
+	}
+	FastLED.show();
 }
 
-void AnimationMode(){
-  ++startIndex;
+void AnimationMode() {
+	++startIndex;
 
-  byte colorIndex = startIndex;
-  for (int i = 0; i < NUM_LEDS; ++i) {
-    leds[i] = ColorFromPalette(currentPalette, colorIndex, 255, currentBlending); // 255 - яркость
-    colorIndex += 1;
-  }
+	byte colorIndex = startIndex;
+	for (int i = 0; i < NUM_LEDS; ++i) {
+		leds[i] = ColorFromPalette(currentPalette, colorIndex, 255, currentBlending); // 255 - яркость
+		colorIndex += 1;
+	}
 
-  FastLED.delay(1000 / 60);
+	FastLED.delay(1000 / 60);
 }
